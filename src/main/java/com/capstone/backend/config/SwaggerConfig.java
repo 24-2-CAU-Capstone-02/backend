5c6406d51f1a6ee106fce0c5ba3bd956503abb0d
동영 김 <kimarang0@naver.com>
2024-10-29 14:37:06 +0900

    feat: Swagger 설정 추가

---
+++ src/main/java/com/capstone/backend/config/SwaggerConfig.java
@@ -0,0 +1,16 @@
+package com.capstone.backend.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Bibim Backend API Document",
+                description = "Swagger로 자동 생성된 비빔 백엔드 API 문서입니다.",
+                version = "v0.1"
+        )
+)
+@Configuration
+public class SwaggerConfig {
+}
