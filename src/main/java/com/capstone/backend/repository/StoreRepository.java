e51658197879b5b18618ec7e4915246c2785d76c
동영 김 <kimarang0@naver.com>
2024-11-05 14:44:46 +0900

    fix: Store 관련 코드 삭제

--- src/main/java/com/capstone/backend/repository/StoreRepository.java
+++
@@ -1,7 +0,0 @@
-package com.capstone.backend.repository;
-
-import com.capstone.backend.entity.Store;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StoreRepository extends JpaRepository<Store, Long> {
-}
