979afc828650b89aaf21a273e0b4eaac4be53cba
Haemulramen <128495883+Haemulramen@users.noreply.github.com>
2024-11-18 17:37:45 +0900

    FEAT: chat gpt 응답 파싱할 데이터 구조 정의

---
+++ src/main/java/com/capstone/backend/dto/response/MenuItemResponse.java
@@ -0,0 +1,16 @@
+package com.capstone.backend.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MenuItemResponse {
+    private String menuName;
+    private String price;
+    private String generalizedName;
+    private String description;
+    private String allergyInfo;
+}
