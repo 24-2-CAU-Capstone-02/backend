0270fa66917bd2e2c6e41281e7ffeb143fce7d39
동영 김 <kimarang0@naver.com>
2024-11-20 23:43:32 +0900

    fix: 로그 한글 오류 수정

--- src/main/java/com/capstone/backend/config/WebConfig.java
+++ src/main/java/com/capstone/backend/config/WebConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.EncodedResourceResolver;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -19,6 +20,8 @@ public class WebConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         // log 파일
         registry.addResourceHandler("/logs/**")
-                .addResourceLocations("file:./logs/");
+                .addResourceLocations("file:./logs/")
+                .resourceChain(true)
+                .addResolver(new EncodedResourceResolver());
     }
 }
